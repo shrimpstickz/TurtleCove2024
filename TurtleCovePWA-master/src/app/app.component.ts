635cbc647c80b67b5c28868dd44979cd529c4372
Michaela Laitinen <kaylait2324@gmail.com>
2024-03-04 20:36:30 -0600

    trying to fix errors ;/
    removed statusbar. trying to fix plugins-service error

--- TurtleCovePWA-master/src/app/app.component.ts
+++ TurtleCovePWA-master/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { InstallCounterService } from './services/install-counter/install-counter.service';
-import { PluginsService } from 'src/app/services/plugins-service/plugins.service';
+import { PluginsService } from 'src/app/services/plugins-service/plugins.service.ts';
 import { Component } from '@angular/core';
 
-import { StatusBar, Style } from '@capacitor/status-bar';
+//import { StatusBar, Style } from '@capacitor/status-bar';
 // removed import { SplashScreen } from '@capacitor/splash-screen'; only needed for mobile app
 
 
@@ -13,17 +13,17 @@ import { StatusBar, Style } from '@capacitor/status-bar';
 export class AppComponent {
 
   constructor(private plugins: PluginsService, private installCounter: InstallCounterService) {
-    this.initializeApp();
+    //this.initializeApp();
   }
 
-  initializeApp() {
+  //initializeApp() {
     // window.addEventListener('appinstalled', () => {
     //   this.installCounter.incrementCount();
     // });
-    StatusBar.setStyle({
-      style: Style.Dark
-    });
-    StatusBar.setBackgroundColor({ color: '#08582e' });
+    //StatusBar.setStyle({
+      //style: Style.Dark
+   // });
+    //StatusBar.setBackgroundColor({ color: '#08582e' });
     // removed SplashScreen.hide();
   }
-}
+
