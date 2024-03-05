e8ef12af481cba019b110fc0cb7a9f30da6ddeae
Michaela Laitinen <kaylait2324@gmail.com>
2024-03-05 16:05:41 -0600

    ignore

--- TurtleCovePWA-master/src/app/pages/gw-tour/gw-stops/gw-stops.module.ts
+++ TurtleCovePWA-master/src/app/pages/gw-tour/gw-stops/gw-stops.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-import  SwiperModule  from 'swiper'; //addeed this
+/// import  SwiperModule  from 'swiper'; //addeed this
 import { IonicModule } from '@ionic/angular';
-import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
+//import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
 import { GwStopsPage } from './gw-stops.page';
 import { GwStopComponent } from './gw-stop/gw-stop.component';
 //import { GwStopViewerComponent } from './gw-stop-viewer/gw-stop-viewer.component';
@@ -19,7 +19,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    NgxUsefulSwiperModule, //added this
+    // NgxUsefulSwiperModule, //added this
     CommonModule,
     FormsModule,
     IonicModule,
